a72de7d8831a98b3c799831fc9a124a513df44f3
JlSanchez05 <jlsanchez2405@gmail.com>
2022-06-19 19:33:37 -0400

    started to play with javascript will get back to it tmw

--- assets/js.js
+++ assets/js.js
@@ -1,6 +1,6 @@
 let currentDay = document.querySelector('#currentDay')
 let hour = document.querySelectorAll('.hour')
-//let txtArea = document.querySelectorAll('#txt') 
+let txtArea = document.querySelectorAll('#txt') 
 let btn = document.querySelectorAll('.btn')
 
 const dateTime = new Date()
@@ -13,22 +13,26 @@ const dateTime = new Date()
 //console.log(currentDay.textContent)
 
 
-if(currentDay.textContent = hour[1].textContent){
-  document.getElementById('txt').style.backgroundColor = "red"
-}else{
-     console.log('the if statement not working ')
+
+
+//for(let i = 0; i < hour.length; i++){
+
+//if(currentDay.textContent === hour[i].textContent){
+  //   document.getElementById('txt').style.backgroundColor = "red"
+  // }else{
+    //    console.log('the if statement not working ')
+   //}
+    // console.log(hour[i].textContent)
+//}
+
+let timeReader = function(){
+  if(currentDay.textContent == hour[1].textContent){
+       console.log('if statement is working')
+
+  }else {
+       console.log('the if statemetn is not working')
+  }
 }
-
-for(let i = 0; i < hour.length; i++){
-
-if(currentDay.textContent = hour[i].textContent){
-     document.getElementById('txt').style.backgroundColor = "red"
-   }else{
-        console.log('the if statement not working ')
-   }
-     console.log(hour[i].textContent)
-}
-
-
-
+timeReader()
+console.log(hour[5].textContent)
 
