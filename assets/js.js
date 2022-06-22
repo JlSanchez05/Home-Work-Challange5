c9f8e5d8af1ff6e3981dcf3cbdd451e372a4d08a
JlSanchez05 <jlsanchez2405@gmail.com>
2022-06-22 18:44:03 -0400

    added the save function to every textarea and did some other edits

--- assets/js.js
+++ assets/js.js
@@ -5,8 +5,26 @@ let hour = document.querySelectorAll('.hour')
 //let txtArea = document.querySelectorAll('#txt') 
 //let btn = document.querySelectorAll('.btn')
 
-let newBtn = document.querySelector('.btnNew')
+let btn0 = document.querySelector('.btn0')
 let txt0 = document.querySelector('#txt0')
+let btn1 = document.querySelector('.btn1')
+let txt1 = document.querySelector('#txt1')
+let btn2 = document.querySelector('.btn2')
+let txt2 = document.querySelector('#txt2')
+let btn3 = document.querySelector('.btn3')
+let txt3 = document.querySelector('#txt3')
+let btn4 = document.querySelector('.btn4')
+let txt4 = document.querySelector('#txt4')
+let btn5 = document.querySelector('.btn5')
+let txt5 = document.querySelector('#txt5')
+let btn6 = document.querySelector('.btn6')
+let txt6 = document.querySelector('#txt6')
+let btn7 = document.querySelector('.btn7')
+let txt7= document.querySelector('#txt7')
+let btn8 = document.querySelector('.btn8')
+let txt8 = document.querySelector('#txt8')
+
+
 
 //const dateTime = new Date()
 //currentDay.textContent = dateTime
@@ -33,15 +51,61 @@ setInterval(() => {
 
 
 
-newBtn.addEventListener('click',function(){
-  let txt0 = document.querySelector('#txt0').value;
+btn0.addEventListener('click',function(){
+  let txt0 = document.querySelector('#txt0').value
   localStorage.setItem('#txt0',txt0)
 
   console.log('clicked')
 })
-  document.querySelector('#txt0').textContent=localStorage.getItem('#txt0');
-
-
+  document.querySelector('#txt0').textContent=localStorage.getItem('#txt0')
+//--
+btn1.addEventListener('click',function(){
+  let txt1 = document.querySelector('#txt1').value
+  localStorage.setItem('#txt1',txt1)
+})
+document.querySelector('#txt1').textContent = localStorage.getItem('#txt1')
+//-
+btn2.addEventListener('click',function(){
+  let txt2 = document.querySelector('#txt2').value
+  localStorage.setItem('#txt2',txt2)
+})
+document.querySelector('#txt2').textContent = localStorage.getItem('#txt2')
+//--
+btn3.addEventListener('click',function(){
+  let txt3 = document.querySelector('#txt3').value
+  localStorage.setItem('#txt3',txt3)
+})
+document.querySelector('#txt3').textContent = localStorage.getItem('#txt3')
+//--
+btn4.addEventListener('click',function(){
+  let txt4 = document.querySelector('#txt4').value
+  localStorage.setItem('#txt4',txt4)
+})
+document.querySelector('#txt4').textContent = localStorage.getItem('#txt4')
+//--
+btn5.addEventListener('click',function(){
+  let txt5 = document.querySelector('#txt5').value
+  localStorage.setItem('#txt5',txt5)
+})
+document.querySelector('#txt5').textContent = localStorage.getItem('#txt5') 
+//--
+btn6.addEventListener('click',function(){
+  let txt6 = document.querySelector('#txt6').value
+  localStorage.setItem('#txt6',txt6)
+})
+document.querySelector('#txt6').textContent = localStorage.getItem('#txt6')
+//--
+btn7.addEventListener('click',function(){
+  let txt7 = document.querySelector('#txt7').value
+  localStorage.setItem('#txt7',txt7)
+})
+document.querySelector('#txt7').textContent = localStorage.getItem('#txt7')
+//--
+btn8.addEventListener('click',function(){
+  let txt8 = document.querySelector('#txt8').value
+  localStorage.setItem('#txt8',txt8)
+})
+document.querySelector('#txt8').textContent = localStorage.getItem('#txt8')
 /*
 function testing(){
   console.log('clicked')
