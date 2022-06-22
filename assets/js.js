4531809e1f03ef5f9a1d3b1ee302a75795fbd809
JlSanchez05 <jlsanchez2405@gmail.com>
2022-06-22 18:19:05 -0400

    added javascript found some bugs that werent going to allow me to work on the save functionality so i changed a couple things

--- assets/js.js
+++ assets/js.js
@@ -2,18 +2,21 @@ const timeElement = document.querySelector('.time')
 const dateElement = document.querySelector('.dates')
 
 let hour = document.querySelectorAll('.hour')
-let txtArea = document.querySelectorAll('#txt') 
-let btn = document.querySelectorAll('.btn')
+//let txtArea = document.querySelectorAll('#txt') 
+//let btn = document.querySelectorAll('.btn')
 
-const dateTime = new Date()
+let newBtn = document.querySelector('.btnNew')
+let txt0 = document.querySelector('#txt0')
+
+//const dateTime = new Date()
 //currentDay.textContent = dateTime
 
 function formatTime(date){
   const hours12 = date.getHours() % 12 || 12
   const minutes = date.getMinutes()
   const isAm = date.getHours()< 12
-
 return `${hours12.toString().padStart(2,' 0')}:${minutes.toString().padStart(2,"0")} ${isAm ? "AM" : "PM"}`
+
 }
 function formatDate(date){
   const DAYS = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
@@ -22,15 +25,30 @@ function formatDate(date){
   return `${DAYS[date.getDay()]}, ${MONTHS[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`
 }
 
-
 setInterval(() => {
   const now = new Date();
   timeElement.textContent = formatTime(now)
   dateElement.textContent = formatDate(now);
-}, 200);
+}, 200)
 
 
 
+newBtn.addEventListener('click',function(){
+  let txt0 = document.querySelector('#txt0').value;
+  localStorage.setItem('#txt0',txt0)
+
+  console.log('clicked')
+})
+  document.querySelector('#txt0').textContent=localStorage.getItem('#txt0');
+
+
+/*
+function testing(){
+  console.log('clicked')
+ 
+}
+ newBtn.addEventListener('click',testing)
+*/
 
 
 //console.log(txtArea)
